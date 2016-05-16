2aa22b2ae8b409d2e606eb4d194ed46340a3d266
Brian Downs <brian.downs@gmail.com>
2016-05-16 10:17:28 -0700

    changed all references of weight to priority in code and comments

---
+++ command/priority.go
@@ -0,0 +1,15 @@
+package command
+
+var Priorities = map[int]string{
+	5: "Not at all important",
+	4: "Slightly important",
+	3: "Moderately important",
+	2: "Somewhat important",
+	1: "Extremely important",
+}
+
+// Priority holds an individual priority
+type Priority struct {
+	Value       int
+	Description string
+}
